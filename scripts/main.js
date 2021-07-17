e421fa23f07abad91a7bacc55339698fd5091c93
Akriti Yadav <94akritiyadav@gmail.com>
2021-07-17 15:02:04 +0530

    Ch7 thumbnail-image to detail transition

--- scripts/main.js
+++ scripts/main.js
@@ -57,8 +57,9 @@ function showDetails() {
   var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
   frame.classList.add(TINY_EFFECT_CLASS);
- frame.classList.remove(TINY_EFFECT_CLASS);
-
+  setTimeout(function() {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
 }
 
 function addKeyPressHandler() {
